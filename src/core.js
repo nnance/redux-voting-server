be1868fb36ab7a814a017028035e1ed27b9671be
Nick Nance <nance.nick@gmail.com>
2015-11-04 22:39:01 -0800

    update core tests

--- src/core.js
+++ src/core.js
@@ -30,11 +30,15 @@ export function next(state) {
 }
 
 export function vote(voteState, entry) {
-  return voteState.updateIn(
-    ['tally', entry],
-    0,
-    tally => tally + 1
-  );
+  if (voteState.get('pair').includes(entry)) {
+    return voteState.updateIn(
+      ['tally', entry],
+      0,
+      tally => tally + 1
+    );
+  } else {
+    return voteState;
+  }
 }
 
 export const INITIAL_STATE = Map();
